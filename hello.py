a7ad2037a6b40a97e456c427bd99ed0ed08668eb
John Kitchin <jkitchin@andrew.cmu.edu>
2022-06-13 12:49:04 +0000

    initial files added

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Adapted from https://flask.palletsprojects.com/en/2.1.x/quickstart/
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
